2d511ccb29c032267d5565dc2ecc46c25743c98f
sbiggs <scott.biggs@consensus-builder.com>
2019-07-17 18:42:09 -0500

    Lots of work on fonts.  Now the menu is a different custom font from the rest of the toolbar.

--- app/src/main/java/sleepfuriously/com/dollargame/view/MainActivity.java
+++ app/src/main/java/sleepfuriously/com/dollargame/view/MainActivity.java
@@ -1,7 +1,22 @@
 package sleepfuriously.com.dollargame.view;
 
+import android.content.Context;
+import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.SpannableStringBuilder;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+import android.support.v7.widget.Toolbar;
+
+import org.w3c.dom.Text;
+
+import java.util.Hashtable;
 
 import sleepfuriously.com.dollargame.R;
 
@@ -15,6 +30,8 @@ public class MainActivity extends AppCompatActivity {
     //------------------------
     //  constants
     //------------------------
+
+    private static final String TAG = MainActivity.class.getSimpleName();
 
     //------------------------
     //  widgets
@@ -32,5 +49,60 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // use my toolbar instead of default
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.build_menu, menu);
+
+        Typeface tf = FontCache.get("fonts/roboto_med.ttf", this);
+
+        // Change the fonts of the menu
+        for (int i = 0; i < menu.size(); i++) {
+            MenuItem menuItem = menu.getItem(i);
+            SpannableStringBuilder ssb = new SpannableStringBuilder(menuItem.getTitle());
+            MyTypefaceSpan span = new MyTypefaceSpan(tf);
+            ssb.setSpan(span, 0, ssb.length(), SpannableStringBuilder.SPAN_EXCLUSIVE_EXCLUSIVE);
+            menuItem.setTitle(ssb);
+        }
+
+
+        return true;
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch (item.getItemId()) {
+            case R.id.build_about:
+                break;
+
+            case R.id.build_help:
+                break;
+
+            case R.id.build_load:
+                break;
+
+            case R.id.build_share:
+                break;
+
+            case R.id.build_solve:
+                break;
+
+            default:
+                Toast.makeText(this, "unknown menu selection", Toast.LENGTH_LONG).show();
+                break;
+        }
+
+        return true;   // end processing (consumed completely)
+    }
+
+
 }
