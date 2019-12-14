40ad5e1ac2f9af9e52fdf75d734ab16b8f54a04d
sbiggs <scott.biggs@consensus-builder.com>
2019-12-13 19:56:46 -0600

    working on. i like the new give and take icons.

--- app/src/main/java/sleepfuriously/com/dollargame/view/PrefsActivity.java
+++ app/src/main/java/sleepfuriously/com/dollargame/view/PrefsActivity.java
@@ -1,17 +1,22 @@
 package sleepfuriously.com.dollargame.view;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.EditTextPreference;
 import android.preference.PreferenceActivity;
 
 import androidx.annotation.Nullable;
 import androidx.preference.PreferenceFragmentCompat;
+
+import java.util.Map;
 
 import sleepfuriously.com.dollargame.R;
 
 /**
  * todo:  describe class' purpose and context
  */
-public class PrefsActivity extends PreferenceActivity {
+public class PrefsActivity extends PreferenceActivity
+        implements SharedPreferences.OnSharedPreferenceChangeListener {
 
     //------------------------
     //  constants
@@ -35,11 +40,12 @@ public class PrefsActivity extends PreferenceActivity {
     //  data
     //------------------------
 
+    SharedPreferences mPrefs;
+
     //------------------------
     //  methods
     //------------------------
 
-    // todo: allow hints to disapper
     // todo: change max and min dollar amount
     // todo: change color scheme???
     // todo: change what the randomize button does
@@ -50,9 +56,52 @@ public class PrefsActivity extends PreferenceActivity {
 
         // This does it directly instead of using a Fragment
         addPreferencesFromResource(R.xml.preference_main);
-
-
     }
 
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        mPrefs = getPreferences(MODE_PRIVATE);
+        mPrefs.registerOnSharedPreferenceChangeListener(this);
+
+        Map<String, ?> prefsMap = mPrefs.getAll();
+        for (Map.Entry<String, ?> prefEntry : prefsMap.entrySet()) {
+            // iterate through the preference entries and update their
+            // summary if they are an instance of EditTextPreference
+            if (prefEntry instanceof EditTextPreference) {
+                updateSummary((EditTextPreference) prefEntry);
+            }
+        }
+    }
+
+
+    @Override
+    protected void onPause() {
+        mPrefs.unregisterOnSharedPreferenceChangeListener(this);
+        super.onPause();
+    }
+
+
+    @Override
+    public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
+        Map<String, ?> prefsMap = mPrefs.getAll();
+
+        // get the preference that has been changed
+        Object changedPref = prefsMap.get(key);
+
+        // and if it's an instance of EditTextPreference class,
+        // update its summary
+        if (prefsMap.get(key) instanceof EditTextPreference) {
+            updateSummary((EditTextPreference) changedPref);
+        }
+    }
+
+
+    private void updateSummary(EditTextPreference pref) {
+        // set the EditTextPreference's summary value to
+        // its current text
+        pref.setSummary(pref.getText());
+    }
 
 }
